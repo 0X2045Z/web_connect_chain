5d9abdbf0f70c4f94297b532ffe802b4c36cd97d
SiriusDao-development <siriusdao911@gmail.com>
2022-09-06 15:15:36 +0800

    Added web3-react/core入门案例

--- web3js_learn/src/App.js
+++ web3js_learn/src/App.js
@@ -1,13 +1,131 @@
-import React, { Component } from 'react'
+// 使用@web3-react链接MetaMask
+// https://juejin.cn/post/6973973235700334606
+// https://learnblockchain.cn/article/4446
 
-import ConnentWallet from './components/connentWallet/ConnentWallet'
-export default class App extends Component {
+import React from 'react'
+import { Web3ReactProvider, useWeb3React, } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useEagerConnect, useInactiveListener } from './hooks'
 
-  render(){
-    return(
-      <div>
-        <ConnentWallet/>
-      </div>
-    )
-  }
+import { Spinner } from './Spiner'
+import { injected } from './connectors/connector.js'
+
+import './App.css';
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 5000
+  return library
 }
+
+function ChainId() {
+  const { chainId, active, account, library, connector, activate, deactivate } = useWeb3React()
+
+  return (
+    <div className="ChainIdWrapper">
+      <span>Chain Id</span>
+      <span role="img" aria-label="chain">
+        ⛓
+      </span>
+      <span className="ChainIdText">{chainId ?? 'Not Connected'}</span><br/>
+      {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+    </div>
+  )
+}
+
+function ConnectChain(props) {
+  const context = useWeb3React()
+  const { connector, library, chainId, account, activate, deactivate, active, error } = context
+
+  const [activatingConnector, setActivatingConnector] = React.useState()
+  React.useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined)
+    }
+  }, [activatingConnector, connector])
+
+  const activating = injected === activatingConnector
+  const connected = injected === connector
+  const disabled = !props.triedEager || !!activatingConnector || !!error
+
+  useInactiveListener(!props.triedEager || !!activatingConnector)
+
+  let isDisconnect = !error && chainId
+  const buttonText = isDisconnect ? 'Disconnect' : (activating ? 'Connectting' : 'Connect' )
+
+  return (
+    <button
+      style={{
+        borderColor: activating ? 'orange' : connected ? 'green' : 'unset',
+        cursor: disabled ? 'unset' : 'pointer',
+        position: 'relative',
+      }}
+      className="ConnectButton"
+      disabled={disabled}
+      onClick={() => {
+        if (!isDisconnect) {
+          setActivatingConnector(injected)
+          activate(injected)
+        } else {
+          deactivate()
+        }
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          color: 'black',
+          margin: '0 0 0 1rem'
+        }}
+      >
+        {activating && <Spinner color={'red'} style={{ height: '50%', marginLeft: '-1rem' }} />}
+      </div>
+      { buttonText }
+    </button>
+  )
+}
+
+function App() {
+  const triedEager = useEagerConnect()
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Counter Example </h1>
+          <ChainId/>
+          <ConnectChain triedEager={triedEager} />
+        </header>
+      </div>
+  );
+}
+
+
+export default function() {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  )
+}
+
+// import React, { Component } from 'react'
+
+// import ConnentButton from './components/page'
+
+// export default class App extends Component {
+
+//   render(){
+//     return(
+//       <div>
+//         <ConnentButton/>
+//       </div>
+//     )
+//   }
+// }
+
+// App.js
---
+++ web3js_learn/src/connectors/connector.js
@@ -0,0 +1,8 @@
+// connector.js
+
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+// 创建连接器对象 并指定了支持的链 id 以包含 Clover 链 id 【1, 3, 4, 5, 42】
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42, 336, 592],
+})
