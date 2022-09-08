9a690686654a08605608d7f0c39596742c4725b2
SiriusDao-development <siriusdao911@gmail.com>
2022-09-08 16:20:26 +0800

    Added

--- web3_react_learn/src/App.js
+++ web3_react_learn/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useWeb3React } from '@web3-react/core'
+import { injected } from './utils/connector'
 
 function App() {
+  const { chainId, active, account, library, connector, activate, deactivate } = useWeb3React()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button 
+        onClick={() => {
+          activate(injected)
+        }}
+      >点击链接</button>
     </div>
   );
 }
